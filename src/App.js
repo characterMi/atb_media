d8cc5064645e522d47a58e57ae338a8822dbf1a5
characterMi <abol1385fx@gmail.com>
2024-12-31 17:55:35 +0330

    fix: fix the lazy loaded components w Suspense

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { Box } from "@mui/material";
-import { lazy } from "react";
+import { lazy, Suspense } from "react";
 import { HashRouter, Route, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar.jsx";
 
@@ -8,16 +8,35 @@ const VideoDetail = lazy(() => import("./components/VideoDetail"));
 const ChannelDetail = lazy(() => import("./components/ChannelDetail"));
 const SearchFeed = lazy(() => import("./components/SearchFeed"));
 
+const RouteWithinSuspense = ({ child }) => (
+  <Suspense
+    fallback={
+      <div style={{ display: "flex", placeContent: "center" }}>Loading...</div>
+    }
+  >
+    {child}
+  </Suspense>
+);
+
 function App() {
   return (
     <HashRouter>
       <Box sx={{ backgroundColor: "#000" }} className="app">
         <Navbar />
         <Routes>
-          <Route path="/" element={<Feed />} />
-          <Route path="/video/:id" element={<VideoDetail />} />
-          <Route path="/channel/:id" element={<ChannelDetail />} />
-          <Route path="/search/:searchTerm" element={<SearchFeed />} />
+          <Route path="/" element={<RouteWithinSuspense child={<Feed />} />} />
+          <Route
+            path="/video/:id"
+            element={<RouteWithinSuspense child={<VideoDetail />} />}
+          />
+          <Route
+            path="/channel/:id"
+            element={<RouteWithinSuspense child={<ChannelDetail />} />}
+          />
+          <Route
+            path="/search/:searchTerm"
+            element={<RouteWithinSuspense child={<SearchFeed />} />}
+          />
         </Routes>
       </Box>
     </HashRouter>
