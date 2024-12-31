176205de4f6e8e8cae6c1b9fdda992bc592df771
characterMi <abol1385fx@gmail.com>
2024-12-31 18:53:53 +0330

    fix: fix ui issues

--- src/components/ChannelDetail.jsx
+++ src/components/ChannelDetail.jsx
@@ -10,13 +10,15 @@ const ChannelDetail = () => {
   const [channelDetail, setChannelDetail] = useState(null);
   const [videos, setVideos] = useState([])
   const { id } = useParams();
+
   useEffect(() => {
     fetchFromAPI(`channels?part=snippet&id=${id}`)
       .then((data) => setChannelDetail(data?.items[0]));
 
     fetchFromAPI(`search?channelId=${id}&part=snippet&order=date`)
       .then((data) => setVideos(data?.items));
-  }, [id])
+  }, [id]);
+
   return (
     <Box minHeight="95vh">
       <Box>
--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -5,33 +5,39 @@ import { demoChannelTitle, demoVideoTitle, demoVideoUrl } from "../utils/constan
 
 const VideoCard = ({ video: { id: { videoId }, snippet } }) => {
   return (
-    <Card
-      component={Link}
-      to={videoId ? `/video/${videoId}` : demoVideoUrl}
-      onClick={() => window.scrollTo({ top: 0, behavior: "smooth" })}
-      sx={{ width: { xs: 'calc(100vw - 50px)', sm: '300px' }, boxShadow: "none", borderRadius: "0", mt: "15px", ml: "15px" }}
-    >
-      <CardMedia
-        alt={snippet?.title}
-        image={snippet?.thumbnails?.high?.url}
-        loading="lazy"
-        sx={{ width: { xs: '100%', sm: "100%" }, aspectRatio: "16/9" }}
-      />
-      <CardContent sx={{ backgroundColor: "#1e1e1e", height: "106px" }}>
-        <Typography
-          variant="subtitle1"
-          fontWeight="bold"
-          color="#fff"
-          dangerouslySetInnerHTML={{
-            __html: snippet?.title.slice(0, 60) || demoVideoTitle.slice(0, 60)
-          }}
+    <Link to={videoId ? `/video/${videoId}` : demoVideoUrl}>
+      <Card
+        onClick={() => window.scrollTo({ top: 0, behavior: "smooth" })}
+        sx={{
+          width: { xs: 'calc(100vw - 50px)', sm: '300px' },
+          boxShadow: "none",
+          borderRadius: "0",
+          mt: "15px",
+          ml: { xs: "0", sm: "15px" }
+        }}
+      >
+        <CardMedia
+          alt={snippet?.title}
+          image={snippet?.thumbnails?.high?.url}
+          loading="lazy"
+          sx={{ width: { xs: '100%', sm: "100%" }, aspectRatio: "16/9" }}
         />
-        <Typography variant="subtitle2" fontWeight="bold" color="gray">
-          {snippet?.channelTitle.slice(0, 60) || demoChannelTitle.slice(0, 60)}
-          <CheckCircle sx={{ fontSize: 12, color: "gray", ml: '5px' }} />
-        </Typography>
-      </CardContent>
-    </Card>
+        <CardContent sx={{ backgroundColor: "#1e1e1e", height: "106px" }}>
+          <Typography
+            variant="subtitle1"
+            fontWeight="bold"
+            color="#fff"
+            dangerouslySetInnerHTML={{
+              __html: snippet?.title.slice(0, 60) || demoVideoTitle.slice(0, 60)
+            }}
+          />
+          <Typography variant="subtitle2" fontWeight="bold" color="gray">
+            {snippet?.channelTitle.slice(0, 60) || demoChannelTitle.slice(0, 60)}
+            <CheckCircle sx={{ fontSize: 12, color: "gray", ml: '5px' }} />
+          </Typography>
+        </CardContent>
+      </Card>
+    </Link>
   )
 }
 
